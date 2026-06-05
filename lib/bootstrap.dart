f0c5b9bb2e2dfa24a1b90e58d76959d522728c66
mohamedelesawy19 <moelesawy19@gmail.com>
2026-06-05 08:20:02 +0300

    feat: integrate Firebase with core configuration and setup

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,11 +2,13 @@
 import 'dart:async';
 
 // Package imports:
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Core imports:
+import '/core/config/firebase/firebase_options.dart';
 import '/core/di/injection_container.dart';
 import '/core/error_handling/global_error_handler.dart';
 import '/core/observers/app_bloc_observer.dart';
@@ -15,13 +17,18 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
 
-    // 1. Setup DI
+    // 1. Initialize Firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // 2. Setup DI
     await InjectionContainer.init();
 
-    // 2. Setup BLoC observer
+    // 3. Setup BLoC observer
     Bloc.observer = const AppBlocObserver();
 
-    // 3. Setup global error handler
+    // 4. Setup global error handler
     FlutterError.onError = GlobalErrorHandler.onFlutterError;
     PlatformDispatcher.instance.onError = GlobalErrorHandler.onPlatformError;
 
